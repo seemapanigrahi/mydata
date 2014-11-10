5a7b1f6120a72265bcf9c53752edef3e0cd0198d
seemapanigrahi <seemapanigrahi92@gmail.com>
2014-11-10 17:58:07 +0530

    my os lab data

---
+++ public_html/smi.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>My Information</title>
+	<meta link rel= "stylesheet" href = "a.css">
+	<meta name="generator" content="Geany 1.23.1" />
+</head>
+
+<body>
+	<p> 
+	<?php
+	$name = "supriti";
+	echo"hello",$name;
+	?>
+	</p>
+	
+</body>
+
+</html>
